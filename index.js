63382f93885c07e7bd3005f628b5e2133530832a
thuwainik <142627387+thuwainik@users.noreply.github.com>
2023-09-11 20:37:51 +0300

    done

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  let BMI = weight / ((height / 100) ^ 2);
+  alert(`Your BMI result is ${BMI}`);
+  if (BMI < 18.5) {
+    alert(`UnderWeight`);
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    alert(`Healthy Weight`);
+  } else if (BMI >= 25 && BMI <= 29.9) {
+    alert(`Over Weight`);
+  } else if (BMI >= 30) {
+    alert(`Obesity`);
+  }
+
+  if (
+    (BMI >= 19 && BMI <= 24 && age >= 19 && age <= 24) ||
+    (BMI >= 20 && BMI <= 25 && age >= 25 && age <= 34) ||
+    (BMI >= 21 && BMI <= 26 && age >= 35 && age <= 44) ||
+    (BMI >= 22 && BMI <= 27 && age >= 45 && age <= 54) ||
+    (BMI >= 23 && BMI <= 28 && age >= 55 && age <= 64) ||
+    (BMI >= 24 && BMI <= 29 && age >= 65)
+  ) {
+    alert(`Healthy`);
+  } else {
+    alert(`Not Healthy`);
+  }
 }
